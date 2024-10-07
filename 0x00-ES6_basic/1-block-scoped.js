d5fc8837cce76c899b2721d32270e598d5768c5d
muzijerrymuller <jerrymuller883@gmail.com.com>
2024-10-07 11:37:44 +0000

    block sdffe

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,8 +3,8 @@ export default function taskBlock(trueOrFalse) {
   const task2 = true;  // Use let for the same reason
 
   if (trueOrFalse) {
-    task = true;      // Update the existing variable
-    task2 = false;    // Update the existing variable
+   let task = true;      // Update the existing variable
+   let task2 = false;    // Update the existing variable
   }
 
   return [task, task2]; // Return the updated values
