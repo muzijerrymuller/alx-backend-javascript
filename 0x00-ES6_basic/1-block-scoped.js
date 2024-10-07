1078085dc51ff0ab7341bb1740c0e4b881f1792c
muzijerrymuller <jerrymuller883@gmail.com.com>
2024-10-07 11:03:27 +0000

    block scope

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,4 +1,5 @@
-// The taskBlock function takes a boolean input, modifies block-scoped tasks based on the input, and returns an array with task states.
+// The taskBlock function takes a boolean input, modifies block-scoped
+// tasks based on the input, and returns an array with task states.
 export default function taskBlock(trueOrFalse) {
   const task = false;
   const task2 = true;
