b64fb3a9287580d0d60648a7dba92bca272833c7
muzijerrymuller <jerrymuller883@gmail.com.com>
2024-10-10 15:37:55 +0000

    promieses

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,16 +1,6 @@
-export default function getFullResponseFromAPI(success) {
-  return new Promise((resolve, reject) => {
-    if (success) {
-      // If success is true, resolve the promise and log the message
-      const response = {
-        status: 200,
-        body: 'Success',
-      };
-      console.log('Got a response from the API'); // Log the response message
-      resolve(response);
-    } else {
-      // If success is false, reject the promise with an empty error object
-      reject(new Error('')); // Return an empty error message
-    }
-  });
+export default function handleResponseFromAPI(promise) {
+  return promise
+    .then(() => ({ status: 200, body: 'success' }))
+    .catch(() => new Error())
+    .finally(() => console.log('Got a response from the API'));
 }
