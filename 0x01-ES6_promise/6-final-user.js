118b4500b74a2fcbf722c45fc85195d0035bcd9d
muzijerrymuller <jerrymuller883@gmail.com.com>
2024-10-11 08:28:48 +0000

    FINAL USER

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,8 +2,12 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default async function handleProfileSignup(firstName, lastName, fileName) {
+  // Break down promises into individual variables for readability
+  const signUpPromise = signUpUser(firstName, lastName);
+  const uploadPhotoPromise = uploadPhoto(fileName);
+
   // Use await with Promise.allSettled to wait for both promises to settle
-  const results = await Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)]);
+  const results = await Promise.allSettled([signUpPromise, uploadPhotoPromise]);
 
   // Map over the results to return the desired structure
   return results.map((result) => ({
