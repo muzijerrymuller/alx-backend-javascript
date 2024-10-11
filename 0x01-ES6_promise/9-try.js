d310cce7d9b164bd7656821504e6aed3ac66d031
muzijerrymuller <jerrymuller883@gmail.com.com>
2024-10-11 13:15:04 +0000

    trying for math equation

---
+++ 0x01-ES6_promise/9-try.js
@@ -0,0 +1,13 @@
+break this code down for me export default function guardrail(mathFunction) {
+  const queue = [];
+
+  try {
+    queue.push(mathFunction());
+  } catch (err) {
+    queue.push(String(err));
+  } finally {
+    queue.push('Guardrail was processed');
+  }
+
+  return queue;
+}
