5151ae94ebd80e0188a994aee3f8f287e1f8a9f3
muzijerrymuller <jerrymuller883@gmail.com.com>
2024-10-15 10:17:23 +0000

    currency

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,33 @@
+class Currency {
+  constructor(code, name) {
+    this._code = code;
+    this._name = name;
+  }
+
+  // Getter for code
+  get code() {
+    return this._code;
+  }
+
+  // Setter for code
+  set code(value) {
+    this._code = value;
+  }
+
+  // Getter for name
+  get name() {
+    return this._name;
+  }
+
+  // Setter for name
+  set name(value) {
+    this._name = value;
+  }
+
+  // Method to display full currency format
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
+}
+
+export default Currency;
