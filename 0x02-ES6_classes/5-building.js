6d57bc2ea7fe6a5080d7d601f70a4b4d51aa241f
muzijerrymuller <jerrymuller883@gmail.com.com>
2024-10-16 13:00:44 +0000

    building

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,25 +1,12 @@
 export default class Building {
   constructor(sqft) {
-    if (new.target === Building) {
-      throw new Error("Cannot instantiate an abstract class.");
+    if (new.target !== Building && this.evacuationWarningMessage === undefined) {
+      throw Error('Class extending Building must override evacuationWarningMessage');
     }
     this._sqft = sqft;
   }
 
-  // Getter for sqft
   get sqft() {
     return this._sqft;
-  }
-
-  // Setter for sqft
-  set sqft(value) {
-    if (typeof value !== 'number' || value <= 0) {
-      throw new Error("sqft must be a positive number");
-    }
-    this._sqft = value;
-  }
-
-  evacuationWarningMessage() {
-    throw new Error("Class extending Building must override evacuationWarningMessage");
   }
 }
