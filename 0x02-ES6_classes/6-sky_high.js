5049fd482368ec6ca553038274c1bb42a9482eb0
muzijerrymuller <jerrymuller883@gmail.com.com>
2024-10-17 11:02:28 +0000

    sky high

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -1,23 +1,20 @@
-// Import Building from 5-building.js
-const Building = require('./5-building.js');
+import Building from './5-building';
 
-class SkyHighBuilding extends Building {
+export default class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
-    // Call the parent class constructor with sqft
     super(sqft);
-    // Store the number of floors in an underscore attribute
-    this._floors = floors;
+    this.floors = floors;
   }
 
-  // Getter for floors
   get floors() {
     return this._floors;
   }
 
-  // Override the evacuationWarningMessage method
+  set floors(value) {
+    this._floors = value;
+  }
+
   evacuationWarningMessage() {
-    return `Evacuate slowly the ${this._floors} floors.`;
+    return `Evacuate slowly the ${this.floors} floors`;
   }
 }
-
-module.exports = SkyHighBuilding;
