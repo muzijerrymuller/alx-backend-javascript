a6bcf1dbfe2c8f0c58f5a2ad8701bef3e2a5cbed
muzijerrymuller <jerrymuller883@gmail.com.com>
2024-10-17 16:06:24 +0000

    listing

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,6 +1,6 @@
-function getListStudentIds(students) {
-    if (!Array.isArray(students)) {
-        return [];
-    }
-    return students.map(student => student.id);
+export default function getListStudentIds(students) {
+  if (students instanceof Array) {
+    return students.map((student) => student.id);
+  }
+  return [];
 }
