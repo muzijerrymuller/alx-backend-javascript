ed3074eaa55ec42e3a73d9a32996476c143ba254
muzijerrymuller <jerrymuller883@gmail.com.com>
2024-10-18 11:36:34 +0000

    update by grsde

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -4,11 +4,11 @@ export default function updateStudentGradeByCity(students, city, newGrades) {
     .map((student) => {
       // Find the grade for the current student in newGrades
       const gradeObj = newGrades.find((grade) => grade.studentId === student.id);
-      
+
       // Return the student object with the updated grade
       return {
         ...student,
-        grade: gradeObj ? gradeObj.grade : 'N/A'  // If grade found, use it; else, use 'N/A'
+        grade: gradeObj ? gradeObj.grade : 'N/A', // If grade found, use it; else, use 'N/A'
       };
     });
 }
