4886c8bf20b1d301c379649c81b825dfe3c93652
muzijerrymuller <jerrymuller883@gmail.com.com>
2024-10-18 13:47:14 +0000

    update uniq item

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,17 +1,6 @@
-function updateUniqueItems(itemsMap) {
-    // Check if the input is a Map
-    if (!(itemsMap instanceof Map)) {
-        throw new Error("Cannot process");
-    }
-
-    // Iterate through the entries of the Map
-    for (const [key, value] of itemsMap) {
-        // Check if the quantity is 1
-        if (value === 1) {
-            // Update the quantity to 100
-            itemsMap.set(key, 100);
-        }
-    }
-
-    return itemsMap;
+export default function updateUniqueItems(map) {
+  if (!(map instanceof Map)) throw Error('Cannot process');
+  const copy = map;
+  for (const [d, l] of map.entries()) if (l === 1) copy.set(d, 100);
+  return copy;
 }
