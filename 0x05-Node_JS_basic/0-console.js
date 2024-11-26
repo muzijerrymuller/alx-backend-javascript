066ee44e50b5f831ff25a5402208d22b7c647e82
muzijerrymuller <jerrymuller883@gmail.com>
2024-11-26 16:56:00 +0000

    console

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -3,26 +3,28 @@
  *
  * @function
  * @param {string} msg - The message to display. Must be a valid string.
- * 
+ *
  * @example
  * // Logs "Hello, world!" to the console
  * displayMessage("Hello, world!");
  *
  * @description
- * This function is a lightweight, reusable utility for displaying messages 
- * to the console via standard output (STDOUT). Perfect for quick debugging 
+ * This function is a lightweight, reusable utility for displaying messages
+ * to the console via standard output (STDOUT). Perfect for quick debugging
  * or providing meaningful runtime information.
  *
  * - Designed with simplicity in mind.
  * - Ensures clear and consistent logging in any application.
  *
  * @author
- * Bezaleel Olakunori  
+ * Bezaleel Olakunori 
  * [GitHub Profile](https://github.com/B3zaleel)
  *
  * @module displayMessage
  * @version 1.0.0
  */
+
+
 const displayMessage = (msg) => {
   console.log(msg);
 };
